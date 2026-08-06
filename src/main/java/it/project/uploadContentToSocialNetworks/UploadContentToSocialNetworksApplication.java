48aa5d8c518b685614b26ac9e20ab7101762416d
Pavel_Khromov <p.khromov@i-teco.ru>
2026-08-06 15:52:50 +0300

    Initial commit

---
+++ src/main/java/it/project/uploadContentToSocialNetworks/UploadContentToSocialNetworksApplication.java
@@ -0,0 +1,13 @@
+package it.project.uploadContentToSocialNetworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadContentToSocialNetworksApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(UploadContentToSocialNetworksApplication.class, args);
+	}
+
+}
---
+++ src/main/java/it/project/uploadContentToSocialNetworks/controller/VideoController.java
@@ -0,0 +1,20 @@
+package it.project.uploadContentToSocialNetworks.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/video")
+public class VideoController {
+
+    @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    public String uploadVideo(@RequestPart MultipartFile video) {
+        String name = video.getName();
+        return name;
+    }
+
+}
